51eaa520db55a6f13390fc05f31924e6e9248388
Fluffy <fluffy.l2032@gmail.com>
2017-11-15 15:17:41 +0100

    go to strict mode, refactor for new story interface

--- server/routes/hypotheses.js
+++ server/routes/hypotheses.js
@@ -1,313 +1,324 @@
-const express = require('express');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("mz/fs");
+const glob = require("glob");
+require("@golab/adaptive-hypotheses/");
 const router = express.Router();
-const mongoose = require('mongoose');
-const fs = require('mz/fs');
-const path = require('path');
-const glob = require('glob');
-
-function globPromise( pattern, options ){
-  return new Promise( (resolve, reject) => {
-    glob( pattern, options, (err, matches) => {
-      if (err)
-        return reject( err );
-      return resolve( matches );
-    })
-  })
+function globPromise(pattern, options) {
+    return new Promise((resolve, reject) => {
+        glob(pattern, options, (err, matches) => {
+            if (err)
+                return reject(err);
+            return resolve(matches);
+        });
+    });
 }
-
 let mongoCollection = process.env.MONGO_COLLECTION || "coding";
-
 mongoose.connect(`mongodb://mongo/${mongoCollection}`);
 // mongoose.connect("mongodb://localhost/coding");
 mongoose.Promise = global.Promise;
-
-let hypothesisSchema = mongoose.Schema({
-    _hypothesisId: String,
-    actor: String,
-    hypothesis: String,
-    reason: String,
-    numberCodes: Number,
-    codes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Code' }]
+let storySchema = new mongoose.Schema({
+    snapshots: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Hypothesis' }],
+    updates: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Hypothesis' }]
 });
-hypothesisSchema.pre('save', function(next) {
-  this.numberCodes = this.codes.length;
-  next();
+let hypothesisSchema = new mongoose.Schema({
+    // IHypothesis
+    approved: Boolean,
+    tested: Boolean,
+    elements: [{
+            text: String,
+            type: String
+        }],
+    fulltext: String,
+    confidence: Number,
+    state: String,
+    showConfidenceMeter: Boolean,
+    // IHypothesisAdaptive
+    feedback: mongoose.SchemaTypes.Mixed,
+    parseResults: { type: mongoose.SchemaTypes.ObjectId, ref: 'Code' },
+    // IHypothesisCoded
+    codeResults: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Code' }]
 });
-
-let codeSchema = mongoose.Schema({
-  _target: { type: mongoose.Schema.Types.ObjectId, ref: 'hypothesis' },
-  coder: String,
-  results: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Criterium' }]
+hypothesisSchema.pre('save', function (next) {
+    this.numberCodes = this.codeResults ? this.codeResults.length : 0;
+    this.fulltext = this.elements.map(element => element.text).join(" ").trim().toLowerCase();
+    next();
 });
-
-let criteriumSchema = mongoose.Schema({
-  _target: { type: mongoose.Schema.Types.ObjectId, ref: 'Code' },
-  test: String,
-  result: Boolean
+let codeSchema = new mongoose.Schema({
+    coder: String,
+    results: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Criterium' }]
 });
-
-let Hypothesis = mongoose.model( "Hypothesis", hypothesisSchema );
-let Code = mongoose.model( "Code", codeSchema );
-let Criterium = mongoose.model( "Criterium", criteriumSchema );
-
+let criteriumSchema = new mongoose.Schema({
+    test: String,
+    result: Boolean
+});
+let Story = mongoose.model("Story", storySchema);
+let Hypothesis = mongoose.model("Hypothesis", hypothesisSchema);
+let Code = mongoose.model("Code", codeSchema);
+let Criterium = mongoose.model("Criterium", criteriumSchema);
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json( "hypotheses work!" );
+router.get('/', function (req, res, next) {
+    res.json("hypotheses work!");
 });
-
 // clear db DISABLE IN PRODUCTION MODE!
-router.get('/clear', function( req, res, next ) {
-  Promise.all( [
-    Hypothesis.remove(),
-    Code.remove(),
-    Criterium.remove()
-  ]).then(
-      res.send( "db cleared" )
-    ).catch( err => {
-      res.status( 500 );
-      res.send( err.message );
+router.get('/clear', function (req, res, next) {
+    Promise.all([
+        Hypothesis.remove({}),
+        Code.remove({}),
+        Criterium.remove({})
+    ]).then(_ => res.send("db cleared")).catch(err => {
+        res.status(500);
+        res.send(err.message);
     });
 });
-
-// seed db DISABLE IN PRODUCTION MODE!
-router.get('/seed', function( req, res, next) {
-
-  // check if db is empty.
-  Hypothesis.find()
-    .then( contents => {
-      if (contents.length){
-        console.log( contents.length, "db not empty" );
-        res.status( 418 );
-        res.send( "db not empty" );
-      } else {
-        seed().then( counts => {
-            res.json( counts );
-          })
-      }
+router.get('/seed', function (req, res, next) {
+    // check if db is empty.
+    Hypothesis.find()
+        .then(contents => {
+        if (contents.length) {
+            console.log(contents.length, "db not empty");
+            res.status(418);
+            res.send("db not empty");
+        }
+        else {
+            seed().then((counts) => {
+                res.json(counts);
+            });
+        }
     });
 });
-
 // get all data. All of it. That's a lot.
-router.get('/hypotheses', function( req, res, next ){
-  Hypothesis
-    .find({})
-    .populate({
-      path: "codes",
-      model: "Code",
-      populate: {
-        path: "results",
-        model: "Criterium"
-      }
-    })
-    .exec()
-    .then( hypotheses => {
-      res.json( hypotheses );
-    })
-})
-
+router.get('/hypotheses', function (req, res, next) {
+    Story
+        .find({})
+        .populate([{
+            path: "snapshots",
+            model: "Hypothesis",
+            populate: [{
+                    path: "codeResults",
+                    model: "Code",
+                    populate: {
+                        path: "results",
+                        model: "Criterium"
+                    }
+                },
+                {
+                    path: "parseResults",
+                    model: "Code",
+                    populate: {
+                        path: "results",
+                        model: "Criterium"
+                    }
+                }]
+        },
+        {
+            path: "updates",
+            model: "Hypothesis",
+            populate: [{
+                    path: "codeResults",
+                    model: "Code",
+                    populate: {
+                        path: "results",
+                        model: "Criterium"
+                    }
+                },
+                {
+                    path: "parseResults",
+                    model: "Code",
+                    populate: {
+                        path: "results",
+                        model: "Criterium"
+                    }
+                }]
+        }])
+        .exec()
+        .then(hypotheses => {
+        res.json(hypotheses);
+    });
+});
 // get current status
-router.get('/status', function( req, res, next ) {
-  Hypothesis
-    .find()
-    .exec()
-    .then( docs => {
-        let counts = {
-          total: docs.length,
-          one: 0,
-          multiple: 0
-        };
-        for ( doc of docs ){
-            let codes = doc.codes.length;
+router.get('/status', function (req, res, next) {
+    Hypothesis
+        .find()
+        .exec()
+        .then(docs => {
+        let counts = {};
+        let percentages = {};
+        for (let doc of docs) {
+            let codes = doc.numberCodes;
             switch (codes) {
-              case 0:
-              case 1:
-                break;
-              
-              case 2:
-                counts.one++;
-                break;
-            
-              default:
-                counts.multiple++;
-                break;
+                case 0:
+                    break;
+                case 1:
+                    counts.one++;
+                    break;
+                default:
+                    counts.multiple++;
+                    break;
             }
         }
-        counts.percentOne = Math.round( counts.one / counts.total * 1000 ) / 10 + "%";
-        counts.percentMultiple = Math.round( counts.multiple / counts.total * 1000 ) / 10 + "%";
-        res.json( counts );
-    }).catch( err => {
-        res.status( 500 );
-        res.json( err );
-    })
-})
-
-router.get('/check/:id', function( req, res, next ){
-    Hypothesis
-      .findById( req.params.id )
-      .populate({
-        path: "codes",
-        model: "Code",
-        populate: {
-          path: "results",
-          model: "Criterium"
-        }
-      }).exec()
-      .then( doc => {
-          res.json( doc );
-      }).catch( err => {
-          res.status( 500 );
-          res.json( err );
-      });
+        percentages.one = Math.round(counts.one / counts.total * 1000) / 10 + "%";
+        percentages.multiple = Math.round(counts.multiple / counts.total * 1000) / 10 + "%";
+        res.json(counts);
+    }).catch(err => {
+        res.status(500);
+        res.json(err);
+    });
 });
-
-router.get('/hypothesis/:coder', function( req, res, next ){
-  console.log( req.params.coder );
+router.get('/check/:id', function (req, res, next) {
     Hypothesis
-      .find()
-      .populate({
-        path: "codes",
+        .findById(req.params.id)
+        .populate([{
+            path: "codeResults",
+            model: "Code",
+            populate: {
+                path: "results",
+                model: "Criterium"
+            }
+        },
+        {
+            path: "parseResults",
+            model: "Code",
+            populate: {
+                path: "results",
+                model: "Criterium"
+            }
+        }])
+        .exec()
+        .then(doc => {
+        res.json(doc);
+    }).catch(err => {
+        res.status(500);
+        res.json(err);
+    });
+});
+router.get('/hypothesis/:coder', function (req, res, next) {
+    console.log(req.params.coder);
+    Hypothesis
+        .find()
+        .populate({
+        path: "codeResults",
         model: "Code"
-      })
-      .where( 'codes.coder' ).ne( req.params.coder )
-      .limit( 10 )
-      .exec()
-      .then( docs => {
-        if (docs){
-          // get 10 records, and return one at random. Should mitigate overlap from people working simultaneously
-          // better solution would be to check out docs and lock them, but screw that - KISS.
-          res.json( { data: docs[Math.floor(Math.random()*docs.length)] } );
-        } else {
-          res.status( 404 );
-          res.send( "no hypotheses" );
+    })
+        .where('codeResults.coder').ne(req.params.coder)
+        .limit(10)
+        .exec()
+        .then(docs => {
+        if (docs) {
+            // get 10 records, and return one at random. Should mitigate overlap from people working simultaneously
+            // better solution would be to check out docs and lock them, but screw that - KISS.
+            res.json({ data: docs[Math.floor(Math.random() * docs.length)] });
         }
-      }).catch( err => {
-        console.error( err );
-        res.status( 500 );
-        res.send( err.message );
-      })
-})
-
-router.post('/code', function( req, res, next ){
-    console.log( req.body );
-    
-    if (!req.body || !req.body.hypothesis || !req.body.hypothesis._id || !req.body.criteria || !req.body.coder ){
-      res.json( {status: false });
-    } else {
-      Hypothesis
-        .findById( req.body.hypothesis._id )
-        .then( hypothesis => {
+        else {
+            res.status(404);
+            res.send("no hypotheses");
+        }
+    }).catch(err => {
+        console.error(err);
+        res.status(500);
+        res.send(err.message);
+    });
+});
+router.post('/code', function (req, res, next) {
+    console.log(req.body);
+    if (!req.body || !req.body.hypothesis || !req.body.hypothesis._id || !req.body.criteria || !req.body.coder) {
+        res.json({ status: false });
+    }
+    else {
+        Hypothesis
+            .findById(req.body.hypothesis._id)
+            .then(doc => {
+            if (!doc)
+                throw "bad request";
+            return doc;
+        })
+            .then(hypothesis => {
             // create and attach new code
-            let codeRecord = new Code({
-              _target: hypothesis._id,
-              coder: req.body.coder
-            });
-            hypothesis.codes.push( codeRecord );            
+            let codeRecord = new Code({ coder: req.body.coder });
+            if (!hypothesis.codeResults)
+                hypothesis.codeResults = [];
+            hypothesis.codeResults.push(codeRecord);
             hypothesis.save();
-
             // for each identical hypothesis, also add this code
-            Hypothesis.where( "hypothesis", hypothesis.hypothesis )
-                      .where( "_id" ).ne( hypothesis._id )
-              .then( identicalHypotheses => {
-                console.log( identicalHypotheses );
-                identicalHypotheses.map( identical => {
-                  identical.codes.push( codeRecord );
-                  identical.save();
-                })
-              })
-
-            // create and attach criteria
-            for ( let criterium of req.body.criteria ){
-                let criteriumRecord = new Criterium({
-                      _target: codeRecord._id,
-                      test: criterium.test,
-                      result: criterium.result
+            Hypothesis
+                .find()
+                .where("fulltext", hypothesis.fulltext)
+                .where("_id").ne(hypothesis._id)
+                .then(identicalHypotheses => {
+                console.log(identicalHypotheses.length + " identical hypotheses found.");
+                identicalHypotheses.map(identical => {
+                    if (!identical.codeResults)
+                        identical.codeResults = [];
+                    identical.codeResults.push(codeRecord);
+                    identical.save();
                 });
-                codeRecord.results.push( criteriumRecord );
+            });
+            // create and attach criteria
+            for (let criterium of req.body.criteria) {
+                let criteriumRecord = new Criterium({
+                    _target: codeRecord._id,
+                    test: criterium.test,
+                    result: criterium.result
+                });
+                codeRecord.results.push(criteriumRecord);
                 criteriumRecord.save();
             }
-
             // finally, save the codeRecord
             codeRecord.save();
-
-            res.json( { status: true, id: hypothesis._id, check: "/api/check/" + hypothesis._id });
-        }).catch( err => {
-            console.error( err );
-            res.json( { status: false, error: err });
-        })
+            res.json({ status: true, id: hypothesis._id, check: "/api/check/" + hypothesis._id });
+        }).catch(err => {
+            console.error(err);
+            res.json({ status: false, error: err });
+        });
     }
-})
-
+});
 /**
- * Read file contents from ../data/*.json and use them to create hypothesis entries and parser codings.
- */
-let seed = function(){
-  return globPromise( "./data/*.json" )
-      .then( files => { console.log( files ); return files })
-      .then( files => { return Promise.all( 
-          files.map( 
-            file => fs.readFile( file, 'utf8' ) // read file
-                      .then( JSON.parse )       // parse as JSON
-                      .then( parseHypotheses )  // parse codes and store
-          ) ) } )
-      .then( countsArray => {
-        let COUNTS = { files: countsArray.length }
-        for ( let counts of countsArray ){
-          for ( let property in counts ){
-            if (!COUNTS[property]) COUNTS[property] = 0
-            COUNTS[property] += counts[property]
-          }
+* Read file contents from ../data/*.json and use them to create hypothesis entries and parser codings.
+*/
+let seed = function () {
+    return globPromise("./data/*.json", {})
+        .then(files => { console.log(files); return files; })
+        .then(files => {
+        return Promise.all(files.map(file => fs.readFile(file, 'utf8') // read file
+            .then(JSON.parse) // parse as JSON
+            .then(parseHypotheses) // parse codes and store
+        ));
+    })
+        .then((countsArray) => {
+        let COUNTS = { files: countsArray.length };
+        for (let counts of countsArray) {
+            for (let property in counts) {
+                if (!COUNTS[property])
+                    COUNTS[property] = 0;
+                COUNTS[property] += counts[property];
+            }
         }
-        console.log( COUNTS )
+        console.log(COUNTS);
         return COUNTS;
-      }); 
+    });
+};
+function parseHypotheses(data) {
+    let counts = {
+        hypotheses: 0,
+        snapshots: 0,
+        updates: 0
+    };
+    for (let id in data) {
+        // create story object
+        let story = data[id];
+        let storyRecord = new Story(story);
+        counts.hypotheses++;
+        // // add all hypotheses
+        // for ( let snapshot of story.snapshots ){
+        // 	// create hypothesis object
+        // 	let hypothesisRecord = new Hypothesis(snapshot);
+        // 	storyRecord.snapshots.push( hypothesisRecord );
+        // 	// add parser codes
+        // 	let parserCodeRecord = new Code( snapshot.parseResults );
+        // }
+    }
+    return counts;
 }
-
-function parseHypotheses( data ) {
-  let counts = {
-    hypotheses: 0,
-    snapshots: 0,
-    criteria: 0
-  }
-
-  for ( let id in data ){
-      counts.hypotheses++;
-
-      let set = data[id];
-      let actor = set.actor.displayName;
-      for ( let snapshot of set.snapshots ){
-          // create hypothesis object
-          let hypothesisRecord = new Hypothesis({
-              _hypothesisId: id,
-              actor: actor,
-              hypothesis: snapshot.text,
-              reason: snapshot.reason
-          });
-
-          // create parser code
-          let parserCodeRecord = new Code({
-              coder: "Parser"
-          });
-          hypothesisRecord.codes.push( parserCodeRecord );
-
-          // create criteria codeSchema
-          for ( let criterium of snapshot.code ){
-              let criteriumRecord = new Criterium({
-                  test: criterium.test,
-                  result: criterium.status
-              });
-              parserCodeRecord.results.push( criteriumRecord );
-              criteriumRecord.save();
-              counts.criteria++;
-          }
-
-          parserCodeRecord.save();
-          hypothesisRecord.save();
-          counts.snapshots++;
-      }
-  }
-  return counts;
-}
-
-
 module.exports = router;
-
